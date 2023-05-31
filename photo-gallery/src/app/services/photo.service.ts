d9ba21258255c19ea3ef71b09971a659f419bd1e
GustavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2023-05-31 06:32:06 -0300

    captured image is success

--- photo-gallery/src/app/services/photo.service.ts
+++ photo-gallery/src/app/services/photo.service.ts
@@ -5,13 +5,30 @@ import {Camera, CameraResultType, CameraSource} from '@capacitor/camera';
   providedIn: 'root'
 })
 export class PhotoService {
+  public photos: UserPhoto[] = [];
 
-  constructor() { }
+  constructor() {
+  }
+  private async savePicture(photo: Photo){}
   public async addNewToGallery(){
     const capturaPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
     })
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+    this.photos.unshift({
+      filepath: "soon..",
+      webviewPath: capturedPhoto.webPath
+    })
   }
 }
+export interface UserPhoto{
+  filepath: string;
+  webviewPath?: string;
+}
+//https://ionicframework.com/docs/angular/your-first-app/saving-photos
