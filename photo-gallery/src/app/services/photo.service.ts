fd240bd90ace53e0ddc5a5742a0140984ce021fc
GustavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2023-05-31 06:01:18 -0300

    adcionando camera

--- photo-gallery/src/app/services/photo.service.ts
+++ photo-gallery/src/app/services/photo.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Camera, CameraResultType, CameraSource} from '@capacitor/camera';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,11 @@ import { Injectable } from '@angular/core';
 export class PhotoService {
 
   constructor() { }
+  public async addNewToGallery(){
+    const capturaPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    })
+  }
 }
